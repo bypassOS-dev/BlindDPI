d2a9b3cefdb628500fa84c985c4f7aa1eeb5d555
bypassOS <joegoldberg110010100@gmail.com>
2026-08-04 21:43:06 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::{io, net::TcpListener};
-
+use std::io;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
 #[tokio::main]
 async fn main() -> std::io::Result<()>{
     let connect = TcpListener::bind("127.0.0.1:8080").await?;
     let welcome = "welcome! Write something:\n".as_bytes();
 
     loop {
-        let (socet, addr) = connect.
+        //let (socet, addr) = connect.
         //=============================================
         let mut answ = String::new();
         io::stdin()
@@ -15,4 +16,13 @@ async fn main() -> std::io::Result<()>{
         //=============================================
 
     }
+}
+async fn send_message(mut socet: TcpStream, text: &str) -> std::io::Result<()> {
+    let data = text.as_bytes();
+    let len = data.len() as u32;
+    let len_bytes = len.to_be_bytes();
+
+    socet.write_all(&len_bytes).await?;
+    socet.write_all(&data).await?;
+    Ok(())
 }
